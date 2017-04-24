6b4b8f415e36da16851dfccad490f8fe9b0ad904
fuzzagrosner <andrew@fuzzproductions.com>
2017-04-24 10:16:47 -0400

    [binding] make the binding componennt a binding register type so we can use as such.

--- kbinding-anko/src/main/java/com/andrewgrosner/kbinding/anko/BindingComponent.kt
+++ kbinding-anko/src/main/java/com/andrewgrosner/kbinding/anko/BindingComponent.kt
@@ -5,8 +5,7 @@ import android.widget.*
 import com.andrewgrosner.kbinding.BindingHolder
 import com.andrewgrosner.kbinding.BindingRegister
 import com.andrewgrosner.kbinding.ObservableField
-import com.andrewgrosner.kbinding.bindings.ViewRegister
-import com.andrewgrosner.kbinding.bindings.bind
+import com.andrewgrosner.kbinding.bindings.*
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
 import java.util.*
@@ -20,9 +19,9 @@ fun <T, Data> BindingComponent<T, Data>.bind(v: RatingBar) = register.bind(v)
 fun <T, Data> BindingComponent<T, Data>.bind(v: SeekBar) = register.bind(v)
 
 abstract class BindingComponent<T, V>(viewModel: V? = null, val register: BindingRegister<V> = BindingHolder(viewModel))
-    : AnkoComponent<T> {
+    : AnkoComponent<T>, BindingRegister<V> {
 
-    var viewModel: V?
+    override var viewModel: V?
         set(value) {
             register.viewModel = value
         }
@@ -30,17 +29,33 @@ abstract class BindingComponent<T, V>(viewModel: V? = null, val register: Bindin
             return register.viewModel
         }
 
-    fun <Input> bind(function: (V) -> ObservableField<Input>) = register.bind(function)
+    override fun <Input> bind(function: (V) -> ObservableField<Input>) = register.bind(function)
 
-    fun <Input> bind(kProperty: KProperty<*>, expression: (V) -> Input) = register.bind(kProperty, expression)
+    override fun <Input> bind(kProperty: KProperty<*>?, expression: (V) -> Input) = register.bind(kProperty, expression)
 
-    fun <Input> bindNullable(kProperty: KProperty<*>? = null, expression: (V?) -> Input) = register.bind(kProperty, expression)
+    override fun <Input> bindNullable(kProperty: KProperty<*>?, expression: (V?) -> Input) = register.bindNullable(kProperty, expression)
 
-    fun <Input> bindSelf(function: (V) -> ObservableField<Input>) = register.bindSelf(function)
+    override fun <Input> bindSelf(function: (V) -> ObservableField<Input>) = register.bindSelf(function)
 
-    fun <Input> bindSelf(kProperty: KProperty<*>, expression: (V) -> Input) = register.bindSelf(kProperty, expression)
+    override fun <Input> bindSelf(kProperty: KProperty<*>, expression: (V) -> Input) = register.bindSelf(kProperty, expression)
 
-    fun <Output, VW : View> bind(v: VW, viewRegister: ViewRegister<VW, Output>) = register.bind(v, viewRegister)
+    override fun <Output, VW : View> bind(v: VW, viewRegister: ViewRegister<VW, Output>) = register.bind(v, viewRegister)
+
+    override fun bindAll() = register.bindAll()
+
+    override fun unbindAll() = register.unbindAll()
+
+    override fun registerBinding(oneWayBinding: OneWayBinding<V, *, *, *, *>) = register.registerBinding(oneWayBinding)
+
+    override fun unregisterBinding(oneWayBinding: OneWayBinding<V, *, *, *, *>) = register.unregisterBinding(oneWayBinding)
+
+    override fun registerBinding(twoWayBinding: TwoWayBinding<V, *, *, *, *>) = register.registerBinding(twoWayBinding)
+
+    override fun unregisterBinding(twoWayBinding: TwoWayBinding<V, *, *, *, *>) = register.unregisterBinding(twoWayBinding)
+
+    override fun registerBinding(oneWayToSource: OneWayToSource<V, *, *, *>) = register.registerBinding(oneWayToSource)
+
+    override fun unregisterBinding(oneWayToSource: OneWayToSource<V, *, *, *>) = register.unregisterBinding(oneWayToSource)
 
     override final fun createView(ui: AnkoContext<T>) = createViewWithBindings(ui).apply { register.bindAll() }
 
